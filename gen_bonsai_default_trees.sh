f7af8a1da7e84d67c1862b59da2f9ae268fc0118
maclin726 <b07902028@csie.ntu.edu.tw>
2024-06-17 12:22:20 +0000

    add scripts for generate bonsai/depths trees

--- gen_bonsai_default_trees.sh
+++ gen_bonsai_default_trees.sh
@@ -1,17 +1,11 @@
+label_tree_dir="label_tree_obj"
+
 dataset="txt eurlex"
-# fanouts=(64 64 16 8 6 4)
-
 # dataset="svm amazoncat-13k"
-# fanouts=(116 116 24 11 7 5)
-
 # dataset="svm wiki10-31k"
-# fanouts=(176 176 32 14 8 6)
-
+# dataset="svm wiki-500k"
 # dataset="svm amazon-670k"
-# fanouts=(819 819 88 29 15 10)
-
 # dataset="svm amazon-3m"
-# fanouts=(1677 1677 142 41 20 12)
 
 for clus in elkan
 do  
@@ -20,7 +14,7 @@ do
         for seed in {0..4}
         do
             echo "python3 tree_space.py build_tree $dataset --cluster $clus --K 100 --dmax 2 --seed $seed"
-            time python3 tree_space.py build_tree $dataset label_tree_obj \
+            time python3 tree_space.py build_tree $dataset $label_tree_dir \
                 --cluster $clus \
                 --K 100 \
                 --dmax 2 \
--- gen_bonsai_depth_trees.sh
+++ gen_bonsai_depth_trees.sh
@@ -1,22 +1,23 @@
-depths=(1 2 3 4 5 6)
+label_tree_dir="label_tree_obj"
+depths=(1 2 3 4 5)
 
-# dataset="txt eurlex"
-# fanouts=(64 64 16 8 6 4)
+dataset="txt eurlex"
+fanouts=(64 16 8 6 4)
 
 # dataset="svm amazoncat-13k"
-# fanouts=(116 116 24 11 7 5)
+# fanouts=(116 24 11 7 5)
 
 # dataset="svm wiki10-31k"
-# fanouts=(176 176 32 14 8 6)
+# fanouts=(176 32 14 8 6)
 
 # dataset="svm amazon-670k"
-# fanouts=(819 819 88 29 15 10)
+# fanouts=(819 88 29 15 10)
 
 # dataset="svm amazon-3m"
-# fanouts=(1677 1677 142 41 20 12)
+# fanouts=(1677 142 41 20 12)
 
-dataset="svm wiki-500k"
-fanouts=(708 708 80 27 14 9)
+# dataset="svm wiki-500k"
+# fanouts=(708 80 27 14 9)
 
 for clus in elkan
 do  
@@ -25,7 +26,7 @@ do
         for seed in {0..4}
         do
             echo "python3 tree_space.py build_tree $dataset --cluster $clus --K ${fanouts[i]} --dmax ${depths[i]} --seed $seed"
-            time python3 tree_space.py build_tree $dataset label_tree_obj \
+            time python3 tree_space.py build_tree $dataset $label_tree_dir \
              --cluster $clus \
              --K "${fanouts[i]}" \
              --dmax "${depths[i]}" \
