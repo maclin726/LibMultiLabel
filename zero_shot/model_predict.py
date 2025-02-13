0d05d541ea12ecd410a70997cdc1180c0242f37b
maclin726 <b07902028@csie.ntu.edu.tw>
2025-02-13 14:14:04 +0000

    implement min and avg strategy for MixedPredictor

--- zero_shot/model_predict.py
+++ zero_shot/model_predict.py
@@ -1,12 +1,15 @@
 """
 Usage:
-    model_predict.py --model_path=<model_path> --train_instance_data_path=<train_data> --test_instance_data_path=<test_data> --label_data_path=<label_data>
+    model_predict.py --model_path=<model_path> \
+    --train_instance_data_path=<train_data> \
+    --test_instance_data_path=<test_data> \
+    --label_feature_path=<label_data>
 
 Options:
     --model_path=<model_path>                Path to the model file (string).
     --train_instance_data_path=<train_data>  Path to the training instance data file (string).
     --test_instance_data_path=<test_data>    Path to the testing instance data file (string).
-    --label_data_path=<label_data>           Path to the label data file (string).
+    --label_feature_path=<label_features>    Path to the label data file (string).
 """
 
 import sys
@@ -16,14 +19,18 @@ import os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(parent_dir)
 
+import math
 import pickle
-import libmultilabel.linear as linear
 import numpy as np
+import scipy.sparse as sparse
+import libmultilabel.linear as linear
 
+from time import time
 from functools import partial
 from docopt import docopt
 from sklearn.datasets import load_svmlight_file
-
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import MultiLabelBinarizer
 
 def load_svm_data(file_path, /, *args, **keywords):
     """
@@ -48,25 +55,54 @@ def predict_values_by_tfidf(instance_tfidf, label_tfidf):
     """
     return (instance_tfidf @ label_tfidf.T).toarray()
 
+def metrics_in_batches(X, y, predictor, unseen_labels, **kargs_for_predictors):
+    batch_size = 256
+    num_instances = X.shape[0]
+    num_batches = math.ceil(num_instances / batch_size)
+
+    metrics = linear.get_metrics(
+        ["P@1", "P@3", "P@5", "R@10", "R@20", "R@100"], 
+        num_classes=y.shape[1]
+    )
+
+    for i in range(num_batches):
+        preds = predictor.predict_on_all_label(
+            X[i * batch_size : (i + 1) * batch_size], 
+            **kargs_for_predictors
+        )
+        target = y[i * batch_size : (i + 1) * batch_size].toarray()
+        metrics.update(preds, target)
+    
+    return metrics.compute()
+
 class MixedPredictor:
     """A mixed predictor can predict on seen and unseen labels."""
 
     def __init__(
             self,
             all_label_map,
-            seen_label_map,
+            seen_labels,
             supervised_model,
             label_feature,
         ):
         self.all_label_map = all_label_map
-        self.seen_label_map = seen_label_map
+        self.seen_labels = seen_labels
         self.supervised_model = supervised_model
         self.label_feature = label_feature
+        self.unseen_labels = np.setdiff1d(all_label_map, seen_labels)
+        assert (
+            self.unseen_labels.shape[0] + self.seen_labels.shape[0] 
+            == self.all_label_map.shape[0]
+        ), "The set of seen labels should be a subset of all labels."
 
-        self.unseen_label_map = np.setdiff1d(all_label_map, seen_label_map)
-        self.unseen_label_feature = label_feature[self.unseen_label_map]
+        self.seen_label_feature = label_feature[self.seen_labels]
+        self.unseen_label_feature = label_feature[self.unseen_labels]
+        self.label_neighbors = self.get_kneighbors() # (n_labels, n_neighbors)
 
-        assert self.unseen_label_map.shape[0] + self.seen_label_map.shape[0] == self.all_label_map.shape[0], "The set of seen labels should be a subset of all labels."
+    def get_kneighbors(self, n_neighbors=5):
+        neigh = NearestNeighbors(n_neighbors=n_neighbors)
+        neigh.fit(self.seen_label_feature)
+        return neigh.kneighbors(self.label_feature, return_distance=False)
 
     def predict_values_on_seen_label(self, x):
         preds = self.supervised_model.predict_values(x)
@@ -76,8 +112,61 @@ class MixedPredictor:
         preds = predict_values_by_tfidf(x, self.unseen_label_feature)
         return preds
 
-    def predict_on_all_label(self, x):
-        raise NotImplementedError
+    def predict_on_all_label(self, x, alpha, beta, proxy_type):
+        """
+        Predict the values for all labels based on the unified framework, given
+        as 
+            scores_seen = alpha * s_hat_seen + (1-alpha) * label_doc_sim_seen
+            scores_unseen = beta * proxy + (1-beta) * label_doc_sim_unseen
+
+            Args:
+                alpha (float): a number in [0, 1]
+                beta (float): a number in [0, 1]
+                proxy_type (str): could be one of the following
+                    "zero": 
+                    "insert_closest":
+                    "avg":
+                    "min":
+                    "period":
+
+        """
+        preds = np.zeros((x.shape[0], self.all_label_map.shape[0]))
+        seen_label_doc_sim = \
+            predict_values_by_tfidf(x, self.seen_label_feature)
+        s_hat_seen = self.predict_values_on_seen_label(x)
+        
+        preds[:,self.seen_labels] = \
+            alpha * s_hat_seen + (1-alpha) * seen_label_doc_sim
+        
+        proxy = np.zeros((x.shape[0], self.unseen_labels.shape[0]))
+        if proxy_type == "zero":
+            pass
+        elif proxy_type == "insert_closest":
+            nearest_seen_label = self.label_neighbors[self.unseen_labels, 0]
+            sign = np.sign(
+                (x @ (self.label_feature[self.unseen_labels] - \
+                self.label_feature[nearest_seen_label]).T).toarray()
+            )
+            proxy = preds[:,nearest_seen_label] + sign * 1e-8
+        elif proxy_type == "avg":
+            nearest_seen_labels = self.label_neighbors[self.unseen_labels, :3]
+            # preds[:,nearest_seen_labels].shape is 
+            # (n_instances, n_unseen labels, n_nearest neighbors)
+            proxy = np.average(preds[:,nearest_seen_labels], axis=2)
+        elif proxy_type == "min":
+            nearest_seen_labels = self.label_neighbors[self.unseen_labels, :3]
+            proxy = np.min(preds[:,nearest_seen_labels], axis=2)
+        # elif proxy_type == "period":
+        #     pass
+        else:
+            raise ValueError("Unknown proxy type for unseen labels")
+
+        unseen_label_doc_sim = \
+            predict_values_by_tfidf(x, self.unseen_label_feature)
+        preds[:,self.unseen_labels] = \
+            beta * proxy + (1-beta) * unseen_label_doc_sim
+        
+        return preds
 
 def main():
     # Parse command-line arguments
@@ -87,36 +176,46 @@ def main():
     model_path = args['--model_path']
     train_data_path = args['--train_instance_data_path']
     test_data_path = args['--test_instance_data_path']
-    label_data_path = args['--label_data_path']
+    label_feature_path = args['--label_feature_path']
     
     # Print the parsed arguments (for debugging purposes)
     print(f"Model Path: {model_path}")
     print(f"Train Instance Data Path: {train_data_path}")
     print(f"Test Instance Data Path: {test_data_path}")
-    print(f"Label Data Path: {label_data_path}")
+    print(f"Label Feature Path: {label_feature_path}")
     
     print("Start processing...")
 
     # Load models and data
-    X_train, y_train = load_svm_data(train_data_path)
-    X_test, y_test = load_svm_data(test_data_path, n_features=X_train.shape[1])
-    X_label, _ = load_svm_data(label_data_path, n_features=X_train.shape[1])
     with open(model_path, "rb") as _F:
         model = pickle.load(_F)['model']
 
+    # model.flat_model.weights.shape: (n_features, n_classifiers)
+    X_train, y_train = load_svm_data(
+        train_data_path, n_features=model.flat_model.weights.shape[0])
+    X_test, y_test = load_svm_data(test_data_path, n_features=X_train.shape[1])
+    X_label, _ = load_svm_data(label_feature_path, n_features=X_train.shape[1])
+
+    binarizer = MultiLabelBinarizer(sparse_output=True)
+    binarizer.fit(y_train + y_test)
+    y_train = binarizer.transform(y_train)
+    y_test = binarizer.transform(y_test)
+    seen_labels = np.nonzero(np.sum(y_train, axis=0)[0])[1]
+    unseen_labels = np.setdiff1d(
+        np.arange(X_label.shape[0], dtype="int"), seen_labels)
+
     # Init a mixed predictor
     mixed_predictor = MixedPredictor(
-            np.arange(X_label.shape[0], dtype="int"),
-            model.root.label_map,
-            model,
-            X_label,
-            )
+        np.arange(X_label.shape[0], dtype="int"),
+        seen_labels,
+        model,
+        X_label,
+    )
 
-    # Predict seen and unseen labels on the test data
-    preds_on_seen_label = mixed_predictor.predict_values_on_seen_label(X_test)
-    preds_on_unseen_label = mixed_predictor.predict_values_on_unseen_label(X_test)
-    print(preds_on_seen_label.shape)
-    print(preds_on_unseen_label.shape)
+    metric_dict = metrics_in_batches(
+        X_test, y_test, mixed_predictor, unseen_labels,
+        alpha=1, beta=0, proxy_type="zero")
+    print(linear.tabulate_metrics(metric_dict, "test"))
 
 if __name__ == "__main__":
     main()
--- zero_shot/run.sh
+++ zero_shot/run.sh
@@ -2,11 +2,12 @@
 
 # Change the data root and model path accordingly
 model_path="../runs/eurlex57k_linear_tree_default_params_20241207110126/linear_pipeline.pickle"
-data_root="../dataset/EURLEX57K/from_hezhe/v1"
+# model_path="../runs/toy_eurlex57k_tree_20250210125133/linear_pipeline.pickle"
+data_root="../data/eurlex57k"
 
 train_instance_data_path="$data_root/eurlex57k_tfidf_train_ext.svm"
 test_instance_data_path="$data_root/eurlex57k_tfidf_test_ext.svm"
-label_data_path="$data_root/eurlex57k_tfidf_lf.svm"
+label_feature_path="$data_root/eurlex57k_tfidf_lf.svm"
 
 task(){
 # Set up train command
@@ -14,7 +15,7 @@ cmd="python model_predict.py"
 cmd="${cmd} --model_path $model_path"
 cmd="${cmd} --train_instance_data_path $train_instance_data_path"
 cmd="${cmd} --test_instance_data_path $test_instance_data_path"
-cmd="${cmd} --label_data_path $label_data_path"
+cmd="${cmd} --label_feature_path $label_feature_path"
 echo $cmd
 
 }
