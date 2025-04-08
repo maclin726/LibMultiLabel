eb57057d2000677aebe73e2632c3fd0e78c5e387
Linh Vu <linhtvu0@gmail.com>
2025-04-08 11:29:51 +0000

    attention, weighted_avg, plot util

--- zero_shot/run_mimic.sh
+++ zero_shot/run_mimic.sh
@@ -3,15 +3,15 @@
 # Change the data root and model path accordingly
 # full dataset
 model_path="../runs/eurlex57k_linear_tree_default_params_20241207110126/linear_pipeline.pickle"
-data_root="../data/eurlex57k"
+data_root="../data/MIMIC"
 
 # toy dataset
 # model_path="../runs/toy_eurlex57k_tree_20250210125133/linear_pipeline.pickle"
 # data_root="../data/toy_eurlex57k"
 
-train_instance_data_path="$data_root/eurlex57k_tfidf_train_ext.svm"
-test_instance_data_path="$data_root/eurlex57k_tfidf_test_ext.svm"
-label_feature_path="$data_root/eurlex57k_tfidf_lf.svm"
+train_instance_data_path="$data_root/MIMIC_tfidf_train_ext.svm"
+test_instance_data_path="$data_root/MIMIC_tfidf_test_ext.svm"
+label_feature_path="$data_root/MIMIC_tfidf_lf.svm"
 
 task(){
 # Set up train command
