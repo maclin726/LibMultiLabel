a8338a5093d7b520f97b96a1620bcd18b5803bf2
Linh Vu <linhtvu0@gmail.com>
2025-06-30 06:13:08 +0000

    updated code for caching batches

--- zero_shot/model_predict.py
+++ zero_shot/model_predict.py
@@ -3,13 +3,17 @@ Usage:
     model_predict.py --model_path=<model_path> \
     --train_instance_data_path=<train_data> \
     --test_instance_data_path=<test_data> \
-    --label_feature_path=<label_data>
+    --label_feature_path=<label_data> \
+    --run_name=<run_name_str> \
+    --strategy=<strategy_str>
 
 Options:
     --model_path=<model_path>                Path to the model file (string).
     --train_instance_data_path=<train_data>  Path to the training instance data file (string).
     --test_instance_data_path=<test_data>    Path to the testing instance data file (string).
     --label_feature_path=<label_features>    Path to the label data file (string).
+    --run_name=<run_name_str>                Name of the run (string).
+    --strategy=<strategy_str>                Strategy for prediction (string).
 """
 import json
 import sys
@@ -24,6 +28,7 @@ import pickle
 import numpy as np
 import scipy.sparse as sparse
 import libmultilabel.linear as linear
+from liblinear.liblinearutil import predict
 
 from time import time
 from functools import partial
@@ -33,6 +38,7 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.optimize import minimize
+import time
 
 import torch
 import torch.nn as nn
@@ -55,17 +61,6 @@ class Head(nn.Module):
         weight = weight.detach().numpy()
         return weight
     
-class MultiHeadAttention(nn.Module):
-    def __init__(self, feature_in, head_size, num_heads):
-        super().__init__()
-        self.heads = nn.ModuleList([Head(feature_in, head_size) for _ in range(num_heads)])
-        self.head_size = head_size
-        self.num_heads = num_heads
-
-    def forward(self, x):
-        weights = [head(x) for head in self.heads]
-        weights = torch.cat(weights, dim=-1)
-        return weights
 
 def load_svm_data(file_path, /, *args, **keywords):
     """
@@ -91,26 +86,44 @@ def predict_values_by_tfidf(instance_tfidf, label_tfidf):
     
     return (instance_tfidf @ label_tfidf.T).toarray()
 
+# def metrics_in_batches(X, y, preds, unseen_labels, metric_list, save_path, **kargs_for_predictors):
+#     batch_size = 128
+#     num_instances = X.shape[0]
+#     num_batches = math.ceil(num_instances / batch_size)
+#     metrics = linear.get_metrics(
+#         metric_list, 
+#         num_classes=y.shape[1],
+#         unseen_labels=unseen_labels
+#     )
+    
+#     for i in range(num_batches):
+#         target = y[i * batch_size : (i + 1) * batch_size].toarray()
+#         metrics.update(preds[i], target)
+#     return metrics.compute()
+        
 
-def metrics_in_batches(X, y, predictor, unseen_labels, **kargs_for_predictors):
-    batch_size = 256
+def metrics_in_batches(X, y, predictor, unseen_labels, metric_list, save_path, **kargs_for_predictors):
+    batch_size = 16
     num_instances = X.shape[0]
     num_batches = math.ceil(num_instances / batch_size)
-
+    print("testing")
     metrics = linear.get_metrics(
-        ["P@1", "P@3", "P@5", 
-         "R@10", "R@20", "R@50",
-         "ZSR@10", "ZSR@20", "ZSR@50"], 
+        metric_list, 
         num_classes=y.shape[1],
         unseen_labels=unseen_labels
     )
-
+    # print(num_batches)
+    # exit()
     for i in range(num_batches):
+        # orignal
         preds = predictor.predict_on_all_label(
-            X[i * batch_size : (i + 1) * batch_size], 
+            X[i * batch_size : (i + 1) * batch_size], save_path=save_path, num_batches=num_batches, batch_num = i,
             **kargs_for_predictors
         )
+        
+        
         target = y[i * batch_size : (i + 1) * batch_size].toarray()
+        # compare
         metrics.update(preds, target)
     
     return metrics.compute()
@@ -124,9 +137,11 @@ class MixedPredictor:
             seen_labels,
             supervised_model,
             label_feature,
+            strategy='raw'
         ):
         self.all_label_map = all_label_map
         self.seen_labels = seen_labels
+        self.strategy = strategy
         self.supervised_model = supervised_model
         self.label_feature = label_feature
         self.unseen_labels = np.setdiff1d(all_label_map, seen_labels)
@@ -138,7 +153,7 @@ class MixedPredictor:
         
         self.seen_label_feature = label_feature[self.seen_labels]
         self.unseen_label_feature = label_feature[self.unseen_labels]
-        print("seen label feature:",self.seen_label_feature.shape)
+        # print("seen label feature:",self.seen_label_feature.shape)
         self.label_neighbors = self.get_kneighbors() # (n_labels, n_neighbors)
         
 
@@ -149,15 +164,13 @@ class MixedPredictor:
 
     def predict_values_on_seen_label(self, x):
         preds = self.supervised_model.predict_values(x)
-        print('prediction shape')
-        print(preds.shape)
         return preds
 
     def predict_values_on_unseen_label(self, x):
         preds = predict_values_by_tfidf(x, self.unseen_label_feature)
         return preds
 
-    def predict_on_all_label(self, x, alpha, beta, proxy_type):
+    def predict_on_all_label(self, x, alpha, beta, proxy_type, save_path=None, num_batches=None, batch_num=None):
         """
         Predict the values for all labels based on the unified framework,
         given as
@@ -175,24 +188,75 @@ class MixedPredictor:
                     "period":
 
         """
-        preds = np.zeros((x.shape[0], self.all_label_map.shape[0]))
-        seen_label_doc_sim = \
-            predict_values_by_tfidf(x, self.seen_label_feature)
-        print('getting shapes seen_label_doc_sim')
-        print('x:',x.shape)
-        print('seen_label_feature shape:',self.seen_label_feature.shape)
-        print('seen_label_doc_sim shape:',seen_label_doc_sim.shape)
-        unseen_label_doc_sim = \
-            predict_values_by_tfidf(x, self.unseen_label_feature)
-            
-        s_hat_seen = self.predict_values_on_seen_label(x)
         
+        preds = np.zeros((x.shape[0], self.all_label_map.shape[0]))
+        
+        save_path = os.path.dirname(save_path)
+        n = num_batches
+        # np.save(f"{save_path}/s_hat_seen.npy", s_hat_seen)
+        # np.save(f"{save_path}/seen_label_doc_sim.npy", seen_label_doc_sim)
+        # np.save(f"{save_path}/unseen_label_doc_sim.npy", unseen_label_doc_sim)
+        # check if the files exists
+        # for i in range(n-1, -1, -1):
+        #     
+        #     if os.path.exists(f"{save_path}/s_hat_seen_{i}.npy"):
+        #         continue
+        #     else:
+        #         break
+        
+        # check if the last files exsist, if all of them exists, good, load them
+        if os.path.exists(f"{save_path}/s_hat_seen_{batch_num}.npy"):
+            print("loading found")
+            print(f"loading the {batch_num} batch")
+            s_hat_seen = np.load(f"{save_path}/s_hat_seen_{batch_num}.npy")
+            seen_label_doc_sim = np.load(f"{save_path}/seen_label_doc_sim_{batch_num}.npy")
+            unseen_label_doc_sim = np.load(f"{save_path}/unseen_label_doc_sim_{batch_num}.npy")
+        else:
+            start_time = time.time()
+            s_hat_seen = self.predict_values_on_seen_label(x)
+            s_hat_seen = 1 / (1 + np.exp(-s_hat_seen))
+            seen_label_doc_sim = predict_values_by_tfidf(x, self.seen_label_feature)
+            unseen_label_doc_sim = predict_values_by_tfidf(x, self.unseen_label_feature)
+            end_time = time.time()
+            print(f"predicting on batch {batch_num} took {end_time - start_time:.2f} seconds")
+            # save the results
+            if save_path is not None:
+                if not os.path.exists(save_path):
+                    os.makedirs(save_path)
+                np.save(f"{save_path}/s_hat_seen_{batch_num}.npy", s_hat_seen)
+                np.save(f"{save_path}/seen_label_doc_sim_{batch_num}.npy", seen_label_doc_sim)
+                np.save(f"{save_path}/unseen_label_doc_sim_{batch_num}.npy", unseen_label_doc_sim)
+            
+            
+        # s_hat_seen = self.predict_values_on_seen_label(x)
+        # s_hat_seen = 1 / (1 + np.exp(-s_hat_seen))
+        # seen_label_doc_sim = predict_values_by_tfidf(x, self.seen_label_feature)
+        # unseen_label_doc_sim = predict_values_by_tfidf(x, self.unseen_label_feature)
+            # np.save(f"{save_path}/s_hat_seen.npy", s_hat_seen)
+            # np.save(f"{save_path}/seen_label_doc_sim.npy", seen_label_doc_sim)
+            # np.save(f"{save_path}/unseen_label_doc_sim.npy", unseen_label_doc_sim)
+            
+        # seen_label_doc_sim = \
+        #     predict_values_by_tfidf(x, self.seen_label_feature)
+        # unseen_label_doc_sim = \
+        #     predict_values_by_tfidf(x, self.unseen_label_feature)
+        # s_hat_seen = self.predict_values_on_seen_label(x)
+        # s_hat_seen = 1 / (1 + np.exp(-s_hat_seen))
+        
+        # print shapes
+        print(f"s_hat_seen shape: {s_hat_seen.shape}"
+              f", seen_label_doc_sim shape: {seen_label_doc_sim.shape}"
+              f", unseen_label_doc_sim shape: {unseen_label_doc_sim.shape}"
+              f", x shape: {x.shape}")
         preds[:,self.seen_labels] = \
             alpha * s_hat_seen + (1-alpha) * seen_label_doc_sim
+            
         
         proxy = np.zeros((x.shape[0], self.unseen_labels.shape[0]))
         
         if proxy_type == "zero":
+            # preds[:,self.unseen_labels] = 0
+            # return preds
             pass
         elif proxy_type == "insert_closest":
             nearest_seen_label = self.label_neighbors[self.unseen_labels, 0]
@@ -279,12 +343,14 @@ class MixedPredictor:
 def main():
     # Parse command-line arguments
     args = docopt(__doc__)
-    
+
     # Accessing the arguments
     model_path = args['--model_path']
     train_data_path = args['--train_instance_data_path']
     test_data_path = args['--test_instance_data_path']
     label_feature_path = args['--label_feature_path']
+    run_name = args['--run_name']
+    strategy = args['--strategy'] if '--strategy' in args else 'raw'
     
     # Print the parsed arguments (for debugging purposes)
     print(f"Model Path: {model_path}")
@@ -294,20 +360,27 @@ def main():
     
     print("Start processing...")
 
+    start_time = time.time()
     # Load models and data
-    print("check point 1")
     with open(model_path, "rb") as _F:
         model = pickle.load(_F)['model']
+    end_time = time.time()
+    print(f"Model loaded in {end_time - start_time:.2f} seconds.")
 
     # model.flat_model.weights.shape: (n_features, n_classifiers)
-    X_train, y_train = load_svm_data(
-        train_data_path, n_features=model.flat_model.weights.shape[0])
+    if model.name == '1vsrest':
+        X_train, y_train = load_svm_data(
+            train_data_path, n_features=model.weights.shape[0])
+    else:
+        X_train, y_train = load_svm_data(
+            train_data_path, n_features=model.flat_model.weights.shape[0])
 
     X_test, y_test = load_svm_data(test_data_path, n_features=X_train.shape[1])
     X_label, _ = load_svm_data(label_feature_path, n_features=X_train.shape[1])
 
     binarizer = MultiLabelBinarizer(
         classes=np.arange(X_label.shape[0], dtype="float"), sparse_output=True)
+    # binarizer = MultiLabelBinarizer(sparse_output=True)
     binarizer.fit(y_train + y_test)
     y_train = binarizer.transform(y_train)
     y_test = binarizer.transform(y_test)
@@ -315,75 +388,56 @@ def main():
     unseen_labels = np.setdiff1d(
         np.arange(X_label.shape[0], dtype="int"), seen_labels)
 
-    print("testing")
     # Init a mixed predictor
     mixed_predictor = MixedPredictor(
         np.arange(X_label.shape[0], dtype="int"),
         seen_labels,
         model,
         X_label,
+        strategy
     )
     
     # a grid search
-    proxy_types = ["insert_closest", "avg", "attention", "weighted_avg"]
-    # proxy_types = ["attention_test"]
-    """
-    Original 
-    """
-    # print("running?")
-    # alphas = [0, 0.25, 0.5, 0.75, 1]
-    # betas = [0, 0.25, 0.5, 0.75, 1]
+    # proxy_types = ["attention"]
+    proxy_types = ["zero","avg","insert_closest"]
+    
     alphas = np.arange(0, 1.1, 0.1)
     betas = np.arange(0, 1.1, 0.1)
-    pr_scores = []
-    zsrs = []
-    alpha_log = []
-    beta_log = []
+    # alphas = [1]
+    # betas = [1]
     
-    logs = {'alpha': [], 'beta': [], 'pr_score': [], 'zsr': []}
-    from datetime import datetime
-    start_time = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+    metric_list = [
+        "P@1", "P@3", "P@5",
+        "R@1", "R@3", "R@5",
+        "NDCG@1", "NDCG@3", "NDCG@5",
+         "ZSR@10", "ZSR@50", "ZSR@100"]
+    
     for proxy_type in proxy_types:
+        logs = {'alpha': [], 'beta': [], 'full_metrics': []}
         for alpha in alphas:
             for beta in betas:
                 if alpha < beta:
                     continue
                 metric_dict = metrics_in_batches(
-                    X_test, y_test, mixed_predictor, unseen_labels,
+                    X_test, y_test, mixed_predictor, unseen_labels, metric_list, model_path,
                     alpha=alpha, beta=beta, proxy_type=proxy_type)
                 
-                pr_score = np.average([metric_dict["P@1"], metric_dict["P@3"], metric_dict["P@5"], metric_dict["R@10"], metric_dict["R@20"], metric_dict["R@50"]])
-                zsr = np.average([metric_dict["ZSR@10"],metric_dict["ZSR@20"],metric_dict["ZSR@50"]])
-
+                # store the metrics to a list then append to full_metrics
+                full_metrics = {}
+                for metric in metric_list:
+                    full_metrics[metric] = metric_dict[metric]
+                    
                 logs['alpha'].append(alpha)
                 logs['beta'].append(beta)
-                logs['pr_score'].append(pr_score)
-                logs['zsr'].append(zsr)
+                logs['full_metrics'].append(full_metrics)
                 
                 print(linear.tabulate_metrics(
                         metric_dict, 
                         f"a={alpha} b={beta}, proxy={proxy_type} Test"), flush=True)
         # save logs as json
         
-        with open(f'logs_{proxy_type}_{start_time}.json', 'w') as f:
+        with open(f'logs_{proxy_type}_{run_name}.json', 'w') as f:
             json.dump(logs, f)
-            
-    # plot the pr_scores and zsrs
-    # import matplotlib.pyplot as plt
-    # plt.figure(figsize=(10, 6))
-    # plt.plot(pr_scores, zsrs, marker='o')   
-    # plt.title('PR Scores vs ZSRs')
-    # plt.xlabel('PR Scores')
-    # plt.ylabel('ZSRs')
-    # plt.grid()
-    # plt.savefig('pr_scores_vs_zsr.png')
-    
-                
-    # metric_dict = metrics_in_batches(
-    #                 X_test, y_test, mixed_predictor, unseen_labels,
-    #                 alpha=0, beta=0, proxy_type='zero')
-
-    # print(linear.tabulate_metrics(metric_dict, f"Test"), flush=True)
 
 if __name__ == "__main__":
     main()
